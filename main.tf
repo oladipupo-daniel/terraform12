47cc6cdada9fb2ebf7be5fd89ea8ec59049bd6e8
Oladipupo Daniel <oladipupodaniel@gmail.com>
2026-09-15 21:09:48 +0100

    feat: add secure free-tier AWS VPC architecture baseline with tfsec compliance

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_vpc" "fin_core_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "fin-core-vpc"
+    Environment = "Production"
+  }
+}
